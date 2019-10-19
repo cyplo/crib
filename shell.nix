e0ad84e28f8ece4dd02f5d7aaafb94a0dda99cfe
Cyryl Płotnicki <cyplo@cyplo.net>
2019-10-19 16:16:11 +0100

    Add minimal shell.nix

---
+++ shell.nix
@@ -0,0 +1,20 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation rec {
+  name = "env";
+  env = buildEnv { name = name; paths = buildInputs; };
+  buildInputs = [
+    python
+    python37Packages.virtualenv
+    python37Packages.pip
+    python37Packages.setuptools
+    python37Packages.shapely
+    python37Packages.scrapy
+    python37Packages.scipy
+    python37Packages.matplotlib
+    python37Packages.numpy
+    python37Packages.geopandas
+    python37Packages.click
+    python37Packages.click-log
+    python37Packages.cffi
+  ];
+}
